73ed655e0519648c823585b482ee6a81b4ecc1d6
thecarney <37676274+thecarney@users.noreply.github.com>
2018-07-18 21:55:36 -0700

    Cleanup Code Flow

--- js/main.js
+++ js/main.js
@@ -14,97 +14,165 @@ function initialize() {
 
 // Main function uses async/await
 function main() {
+    // globals
+    var attrArray = ["pctWhite", "pctMinority", "pctUnder30Min", "pct30to60Min", "pctOver60Min",
+        "pctBachelorsOrHigher", "pctInPoverty", "perCapitaIncome", "pctRentalRate"];
+    var expressed = attrArray[5]; // per capita income
 
-    // make viz
-    makeViz();
+    // load data
+    loadData();
 
     // async/wait function to load data and draw the visualization
-    async function makeViz() {
+    async function loadData() {
         // make viz responsive
-        d3.select(window)
-            .on("resize", drawFeatures);
+        $(window).resize(function(){
+            try {
+                drawFeatures(dataTable, featCollBlockGroups, featuresBlockGroups, featCollCalifornia,
+                    featuresCalifornia, featuresFrame, expressed, map);
+            } catch(e) {
+                //pass
+            }
+        });
 
         // load data from csv
-        const dataTable = await d3.csv("data/tabularData.csv");
+        let dataTable = await d3.csv("data/tabularData.csv");
 
         // load topologies
-        const topologyBlockGroups = await d3.json("data/LivermoreBlockGroups.topojson");
-        const topologyCalifornia = await d3.json("data/California.topojson");
-        const topologyFrame = await d3.json("data/Inset.topojson");
+        let topologyBlockGroups = await d3.json("data/LivermoreBlockGroups.topojson");
+        let topologyCalifornia = await d3.json("data/California.topojson");
+        let topologyFrame = await d3.json("data/Inset.topojson");
 
         // get feature collections and features if needed
-          // block groups for viz
-        var featCollBlockGroups = topojson
+        // block groups for viz
+        let featCollBlockGroups = topojson
             .feature(topologyBlockGroups, topologyBlockGroups.objects.LivermoreBlockGroups_4326);
-        var featuresBlockGroups = featCollBlockGroups.features;
-          // for inset map
-        var featCollCalifornia = topojson
+        let featuresBlockGroups = featCollBlockGroups.features;
+        // for inset map
+        let featCollCalifornia = topojson
             .feature(topologyCalifornia, topologyCalifornia.objects.California);
-        var featuresCalifornia = featCollCalifornia.features;
-        var featCollFrame = topojson
+        let featuresCalifornia = featCollCalifornia.features;
+        let featCollFrame = topojson
             .feature(topologyFrame, topologyFrame.objects.Inset);
-        var featuresFrame = featCollFrame.features;
+        let featuresFrame = featCollFrame.features;
+
+        // append csv attributes to geojson
+        for (let i = 0; i < dataTable.length; i++) {
+            let tblRow = dataTable[i]; // get row
+            let rowID = tblRow.GEOID; // get row ID
+            for (let a = 0; a < featuresBlockGroups.length; a++) {
+                let featProperties = featuresBlockGroups[a].properties; // get feature
+                let featID = featProperties.GEOID_Data; // get feature ID
+                if (featID == rowID) { // on match...
+                    attrArray.forEach(function (attr) {
+                        let val = parseFloat(tblRow[attr]); // get attribute
+                        featProperties[attr] = val; // set attribute
+                    });
+                }
+            }
+        }
 
         // make main viz container
-        let width = parseInt(d3.select("#mainCardMap").style('width'));
-        let height = parseInt(d3.select("#mainCardMap").style('height'));
-        var map = d3.select("#mainCardMap")
+        let map = d3.select("#mainCardMap")
             .append("svg")
-            .attr("class","map")
+            .attr("class", "map")
             .attr("width", '100%')  //width
             .attr("height", '100%')  //height
             .append("g");
 
-        // call function that draws features
-        drawFeatures();
-
-        // draws features, including on resize
-        function drawFeatures() {
-            // block groups
-            let w1 = parseInt(d3.select("#mainCardMap").style('width'));
-            let h1 = parseInt(d3.select("#mainCardMap").style('height'));
-            let proj1 = d3.geoAlbersUsa().scale(1).fitExtent([[5, 5], [w1, h1]], featCollBlockGroups);
-            let geoPath1 = d3.geoPath().projection(proj1);
-            //d3.select("g").selectAll("path").remove();
-            d3.select(".map").select("g").selectAll("path").remove();  // clear features
-            let blockGroups = map.selectAll(".blockGroups") // draw features again
-                .data(featuresBlockGroups)
-                .enter()
-                .append("path")
-                .attr("d", geoPath1)
-                .attr("class", function(d){
-                    return "blockGroups " + d.properties.GEOID_Data;
-                });
-
-            // inset base
-            let w2 = 50;
-            let h2 = 100;
-            let t = h1-h2;
-            let proj2 = d3.geoAlbersUsa().scale(1).fitExtent([[1, 1], [w2, h2]], featCollCalifornia);
-            let geoPath2 = d3.geoPath().projection(proj2);
-            let california = map.selectAll(".california")
-                .data(featuresCalifornia)
-                .enter()
-                .append("path")
-                .attr("d", geoPath2)
-                .attr("class","california")
-                .attr("transform", "translate(5,"+t+")");
-            let frame = map.selectAll(".frame")
-                .data(featuresFrame)
-                .enter()
-                .append("path")
-                .attr("d", geoPath2)
-                .attr("class", "frame")
-                .attr("transform", "translate(5,"+t+")");
-        }
+        // make map
+         drawFeatures(dataTable, featCollBlockGroups, featuresBlockGroups, featCollCalifornia, featuresCalifornia,
+             featuresFrame, expressed, map);
     }
 }
 
-// load other JS files if needed
-function getExternal() {
-    $.getScript("js/geojson.js", function() {
-        console.log("external script loaded");
-    });
+// draws features, including on resize
+function drawFeatures(dataTable, featCollBlockGroups, featuresBlockGroups, featCollCalifornia,
+                      featuresCalifornia, featuresFrame, expressed, map) {
+    // color scale
+    let colorScale = makeColorScale(dataTable, expressed);
+
+    // block groups
+    let w1 = parseInt(d3.select("#mainCardMap").style('width'));
+    let h1 = parseInt(d3.select("#mainCardMap").style('height'));
+    let proj1 = d3.geoAlbersUsa().scale(1).fitExtent([[5, 5], [w1, h1]], featCollBlockGroups);
+    let geoPath1 = d3.geoPath().projection(proj1);
+
+    // clear old
+    d3.select(".map").select("g").selectAll("path").remove();
+
+    // draw again
+    let blockGroups = map.selectAll(".blockGroups")
+        .data(featuresBlockGroups)
+        .enter()
+        .append("path")
+        .attr("d", geoPath1)
+        .attr("class", function(d){
+            return "blockGroups " + d.properties.GEOID_Data;
+        })
+        .style("fill", function(d){
+            return colorScale(d.properties[expressed]);
+        });
+
+    // inset base
+    let w2 = 50;
+    let h2 = 100;
+    let t = h1-h2;
+    let proj2 = d3.geoAlbersUsa().scale(1).fitExtent([[1, 1], [w2, h2]], featCollCalifornia);
+    let geoPath2 = d3.geoPath().projection(proj2);
+    let california = map.selectAll(".california")
+        .data(featuresCalifornia)
+        .enter()
+        .append("path")
+        .attr("d", geoPath2)
+        .attr("class","california")
+        .attr("transform", "translate(5,"+t+")");
+    let frame = map.selectAll(".frame")
+        .data(featuresFrame)
+        .enter()
+        .append("path")
+        .attr("d", geoPath2)
+        .attr("class", "frame")
+        .attr("transform", "translate(5,"+t+")");
 }
 
+// color scale getter
+function makeColorScale(data, expressed){
+    // available ranges
+    let green = ["#edf8e9","#bae4b3","#74c476","#31a354","#006d2c"]; // income
+    let gray = ["#f7f7f7","#cccccc","#969696","#636363","#252525"]; // poverty
+    let blue = ["#eff3ff","#bdd7e7","#6baed6","#3182bd","#08519c"]; // education
+    let red = ["#fee5d9","#fcae91","#fb6a4a","#de2d26","#a50f15"]; // commute time
+    let purple = ["#f2f0f7","#cbc9e2","#9e9ac8","#756bb1","#54278f"]; // race
+    let orange = ["#feedde","#fdbe85","#fd8d3c","#e6550d","#a63603"]; // rental rate
 
+    // get color for expressed attr
+    let colorArrays = ["pctWhite",purple,"pctMinority",purple,"pctUnder30Min",red,
+        "pct30to60Min",red,"pctOver60Min",red,"pctBachelorsOrHigher",blue,
+        "pctInPoverty",gray,"perCapitaIncome",green,"pctRentalRate",orange];
+    let chosenColor = colorArrays[colorArrays.indexOf(expressed)+1];
+
+    // d3 scale
+    let colorScale = d3.scaleThreshold().range(chosenColor);
+
+    // array of attribute's values
+    let domainArray = [];
+    for (let i=0; i<data.length; i++) {
+        let val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    }
+
+    // clustering
+    let clusters = ss.ckmeans(domainArray, 5);
+
+    // reset to mins
+    domainArray = clusters.map(function(d){
+        return d3.min(d);
+    });
+
+    // remove first value
+    domainArray.shift();
+
+    // last 4 mins to domain
+    colorScale.domain(domainArray);
+    return colorScale;
+}
